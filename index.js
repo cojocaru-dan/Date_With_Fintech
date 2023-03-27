6d2343cf42e4d9697c2a876cea2f5e29edd0c0f1
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-03-27 15:37:44 -0700

    Use JSON data and Date object in Javascript

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 import * as fs from 'node:fs';
 
 let DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// 1 and 2 and 3
+const getDayOfWeek = (dayNr) => DAYS[dayNr];
+
+const holidaysByYear = function(eventsArr) {
+  const numberOfHolidaysByYear = {};
+
+  for (const event of eventsArr) {
+    if (numberOfHolidaysByYear.hasOwnProperty(event.date.year)) {
+      numberOfHolidaysByYear[event.date.year] += 1;
+    } else {
+      numberOfHolidaysByYear[event.date.year] = 1;
+    }
+  }
+  return numberOfHolidaysByYear;
+}
+
+const dataRead = (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const regions = JSON.parse(data);
+
+  for (const region in regions) {
+    for (const event of regions[region].events) {
+      const currentDate = new Date(event.date);
+      event.date = {
+        year: currentDate.getUTCFullYear(),
+        month: currentDate.getUTCMonth(),
+        day: currentDate.getUTCDate(),
+        weekday: getDayOfWeek(currentDate.getDay())
+      }
+      console.log(event);
+    }
+      
+  }
+
+  for (const division in regions) {
+    regions[division]['amount-by-year'] = holidaysByYear(regions[division].events);
+  }
+
+  console.log(regions);
 
 
+}
 
+fs.readFile('data.json', 'utf8', dataRead);
 
 // DON'T MODIFY THE CODE BELOW THIS LINE
 
